1165288f199330e38085a6d8924aa449641b6608
MARLE Sebastien <BZ8036@equans.com>
2023-04-02 17:00:40 +0200

    lastupdate

--- ECOM/index.js
+++ ECOM/index.js
@@ -1,12 +1,40 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import mysql from 'mysql2';
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: 3306,
+    database: 'ecom',
+    password: '',
+}).once('connection',(stream) => {
+    console.log('Ah, we have our first user!');
+});
 
 // initialisation de l'application Express
 const app = express();
 
+
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+
+
+app.get('/', function(req, res) {
+    connection.query('select * from produits', (err, result, fields) => {
+        console.log(result)
+        res.render('pages/index', {produits: result})
+    })
+})
+
+app.get('/panier', function(req, res) {
+    connection.query('select * from panier', (err, result2, fields) => {
+        console.log(result2)
+        res.render('pages/panier', {panier: result2})
+    })
+})
 
 
 
@@ -31,6 +59,15 @@ app.get('/inscription', (req, res) => {
 
 app.get('/boosformulaire', (req, res) => {
     res.render('pages/boosformulaire')
+})
+
+
+app.post('/panier', (req, res) => {
+    console.log(req.body)
+    connection.query('select * from produits', (err, result, fields) => {
+        console.log(result)
+        res.render('pages/index', {produits: result})
+    })
 })
 
 
