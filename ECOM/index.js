302a933d14ddc2cabc9b9716f781282b4bd2ba7d
MARLE Sebastien <BZ8036@equans.com>
2023-04-04 21:06:44 +0200

    paniertest

--- ECOM/index.js
+++ ECOM/index.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
     port: 3306,
     database: 'ecom',
     password: '',
-}).once('connection',(stream) => {
+}).once('connection', (stream) => {
     console.log('Ah, we have our first user!');
 });
 
@@ -17,24 +17,26 @@ const connection = mysql.createConnection({
 const app = express();
 
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     connection.query('select * from produits', (err, result, fields) => {
         console.log(result)
-        res.render('pages/index', {produits: result})
+        res.render('pages/index', { produits: result })
     })
 })
 
-app.get('/panier', function(req, res) {
-    connection.query('select * from panier', (err, result2, fields) => {
-        console.log(result2)
-        res.render('pages/panier', {panier: result2})
+app.get('/panier', function (req, res) {
+    connection.query('select * from panier', (err, result, fields) => {
+        console.log(result)
+        res.render('pages/panier', { panier: result })
     })
 })
+
+
 
 
 
@@ -43,7 +45,7 @@ app.get('/panier', function(req, res) {
 
 // creation d'une 'route' avec la methode http
 //Get dont le chemin est '/'
-app.get('/', function(httpRequest, httpResponse) {
+app.get('/', function (httpRequest, httpResponse) {
     // log de la requete entrante
     // envoie de la réponse http
     httpResponse.render('pages/index')
@@ -62,13 +64,22 @@ app.get('/boosformulaire', (req, res) => {
 })
 
 
-app.post('/panier', (req, res) => {
-    console.log(req.body)
-    connection.query('select * from produits', (err, result, fields) => {
-        console.log(result)
-        res.render('pages/index', {produits: result})
+
+
+app.post('/', function (req, res, next) {
+    let data = req.body;
+    connection.query(`INSERT INTO panier (id, user_id, item_name, price) VALUES (NULL, '${data.user_id}', '${data.item_name}', '${data.price}');`, (err, result, fields) => { });
+    connection.query('SELECT * FROM produits', (err, result, fields) => {
+        res.render('pages/index', { produits: result })
     })
 })
 
+app.post('/panier', function (req, res, next) {
+    let data = req.body;
+    connection.query(`INSERT INTO panier (id) VALUES (NULL, '${data.user_id}');`, (err, result, fields) => { });
+    connection.query('SELECT * FROM panier', (err, result, fields) => {
+        res.render('pages/panier', { panier: result })
+    })
+})
 
 app.listen('3003');
